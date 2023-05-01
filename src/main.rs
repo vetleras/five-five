3769e83b2a897e2e5108138692bb5400a649fb1e
vetleras <vetleras@gmail.com>
2023-05-01 22:37:48 +0200

    Remove solver struct

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ impl Word {
         let bytes: [u8; 5] = bytes.try_into().ok()?;
         let mut bitword = 0;
         let mut len = 0;
-        for letter in bytes.iter().cloned() {
+        for letter in bytes {
             debug_assert!(letter >= b'a');
             debug_assert!(letter <= b'z');
             let offset = letter - b'a';
@@ -64,87 +64,105 @@ fn next_free_letter(filter: u32) -> Option<usize> {
     (0..26).rev().filter(|n| filter & (1 << n) == 0).next()
 }
 
-struct Solver {
-    words: [Vec<Word>; 26],
-    output: Mutex<File>,
+type WordIndex = [Vec<Word>; 26];
+
+fn create_word_index(mut words: Vec<Word>) -> WordIndex {
+    words.par_sort_unstable_by_key(|w| w.bitword);
+    words.dedup_by_key(|w| w.bitword);
+
+    let mut freqs = [0; 26];
+    for word in &words {
+        for b in word.bytes {
+            freqs[(b - b'a') as usize] += 1;
+        }
+    }
+
+    // create transform where least frequent letter is 25, second least 24, ..., most frequent 0
+    let transform: [usize; 26] = freqs
+        .into_iter()
+        .enumerate()
+        .sorted_unstable_by_key(|(_i, f)| *f)
+        .rev()
+        .map(|(i, _f)| i) // letters now in sorted order from most to least frequent
+        .enumerate()
+        .sorted_unstable_by_key(|(_i_transformed, i_letter)| *i_letter) // sort again, s.t. the letter corresponds with index in transform
+        .map(|(i_transformed, _i_letter)| i_transformed)
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+
+    let mut word_index: [Vec<Word>; 26] = Default::default();
+    for word in words {
+        let (msl, word) = word.transform(transform);
+        word_index[msl].push(word);
+    }
+    word_index
 }
 
-impl Solver {
-    fn new(mut words: Vec<Word>, output: File) -> Self {
-        words.par_sort_unstable_by_key(|w| w.bitword);
-        words.dedup_by_key(|w| w.bitword);
+fn solve(words: Vec<Word>, output: File) {
+    let word_index = create_word_index(words);
+    let output = Mutex::new(output);
 
-        let mut freqs = [0; 26];
-        for word in &words {
-            for b in word.bytes {
-                freqs[(b - b'a') as usize] += 1;
+    word_index[25].par_iter().for_each(|word| {
+        let mut solution: [Word; 5] = Default::default();
+        solution[0] = word.clone();
+        solve14(&word_index, &output, word.bitword, false, &mut solution, 1);
+    });
+
+    word_index[24].par_iter().for_each(|word| {
+        let mut solution: [Word; 5] = Default::default();
+        solution[0] = word.clone();
+        solve14(
+            &word_index,
+            &output,
+            word.bitword | 1 << 25,
+            true,
+            &mut solution,
+            1,
+        );
+    });
+}
+
+fn solve14(
+    word_index: &WordIndex,
+    output: &Mutex<File>,
+    filter: u32,
+    skipped: bool,
+    solution: &mut [Word; 5],
+    i: usize,
+) {
+    let letter = next_free_letter(filter).unwrap();
+    if i == 4 {
+        for word in &word_index[letter] {
+            if word.bitword & filter == 0 {
+                writeln!(
+                    output.lock().unwrap(),
+                    "{} {} {} {} {word}",
+                    solution[0],
+                    solution[1],
+                    solution[2],
+                    solution[3]
+                )
+                .unwrap();
             }
         }
-
-        // create transform where least frequent letter is 25, second least 24, ..., most frequent 0
-        let transform: [usize; 26] = freqs
-            .into_iter()
-            .enumerate()
-            .sorted_unstable_by_key(|(_i, f)| *f)
-            .rev()
-            .map(|(i, _f)| i) // letters now in sorted order from most to least frequent
-            .enumerate()
-            .sorted_unstable_by_key(|(_i_transformed, i_letter)| *i_letter) // sort again, s.t. the letter corresponds with index in transform
-            .map(|(i_transformed, _i_letter)| i_transformed)
-            .collect::<Vec<_>>()
-            .try_into()
-            .unwrap();
-
-        let mut indexed_words: [Vec<Word>; 26] = Default::default();
-        for word in words {
-            let (msl, word) = word.transform(transform);
-            indexed_words[msl].push(word);
-        }
-
-        Solver {
-            words: indexed_words,
-            output: Mutex::new(output),
+    } else {
+        for word in &word_index[letter] {
+            if word.bitword & filter == 0 {
+                solution[i] = word.clone();
+                solve14(
+                    word_index,
+                    output,
+                    filter | word.bitword,
+                    skipped,
+                    solution,
+                    i + 1,
+                );
+            }
         }
     }
-
-    fn solve(&self) {
-        self.words[25].par_iter().for_each(|word| {
-            let mut solution: [Word; 5] = Default::default();
-            solution[0] = word.clone();
-            self.solve14(word.bitword, false, &mut solution, 1);
-        });
-
-        self.words[24].par_iter().for_each(|word| {
-            let mut solution: [Word; 5] = Default::default();
-            solution[0] = word.clone();
-            self.solve14(word.bitword | 1 << 25, true, &mut solution, 1);
-        });
-    }
-
-    fn solve14(&self, filter: u32, skipped: bool, solution: &mut [Word; 5], i: usize) {
-        let letter = next_free_letter(filter).unwrap();
-        match i {
-            4 => {
-                for word in &self.words[letter] {
-                    if word.bitword & filter == 0 {
-                        let mut file = self.output.lock().unwrap();
-                        let s = &solution;
-                        writeln!(file, "{} {} {} {} {word}", s[0], s[1], s[2], s[3]).unwrap();
-                    }
-                }
-            }
-            _ => {
-                for word in &self.words[letter] {
-                    if word.bitword & filter == 0 {
-                        solution[i] = word.clone();
-                        self.solve14(filter | word.bitword, skipped, solution, i + 1);
-                    }
-                }
-            }
-        }
-        if !skipped {
-            self.solve14(filter | 1 << letter, true, solution, i);
-        }
+    if !skipped {
+        solve14(word_index, output, filter | 1 << letter, true, solution, i);
     }
 }
 
@@ -156,8 +174,7 @@ fn main() -> Result<()> {
         .collect();
 
     let output = File::create("solutions.txt")?;
-    let solver = Solver::new(words, output);
-    solver.solve();
+    solve(words, output);
 
     println!("{} us", start.elapsed().as_micros());
     Ok(())
